0d39bdd0e5e7d11b1dbcd3770110841a0cffacfd
Mahmudul Hasan <mclean.hasan@gmail.com>
2026-02-17 01:59:58 +0600

    feat(users): add User model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from apps.core.models import BaseModel 
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin 
+from .managers import UserManager
 
-# Create your models here.
+class User(BaseModel, AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email 
