14b462f54041d09f7e88cf9fc957a80dc89147dd
Mahmudul Hasan <mclean.hasan@gmail.com>
2026-02-17 11:37:03 +0600

    install rest framework and set auth user model

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # thir party
+    'rest_framework',
     # custom
     'apps.core',
     'apps.users',
@@ -116,3 +118,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'apps.users.User'
