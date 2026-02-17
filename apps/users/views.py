a84d2301d3c03a029ee7fc121ffcdb626da386ce
Mahmudul Hasan <mclean.hasan@gmail.com>
2026-02-17 12:12:06 +0600

    feat(users): add RegisterView using CreateAPIView

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status, generics 
+from rest_framework.response import Response 
+from .serializers import RegisterSerializer 
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer 
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                "user": RegisterSerializer(user).data,
+                "message": "User created successfully. Use the UID for future lookups",
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
