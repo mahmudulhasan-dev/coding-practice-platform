fcd2aea92cd95ce45077fce305ee8d87c040afc7
Mahmudul Hasan <mclean.hasan@gmail.com>
2026-02-20 00:36:46 +0600

    feat(problems): add category and problem models

--- apps/problems/models.py
+++ apps/problems/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils.text import slugify 
+from apps.core.models import BaseModel 
 
-# Create your models here.
+class Category(BaseModel):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Categories" 
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name 
+    
+class Problem(BaseModel):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='problems'
+    )
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+    solution = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title 
