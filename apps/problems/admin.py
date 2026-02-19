19930d71633aa3b68d922c45ea577622e3c52feb
Mahmudul Hasan <mclean.hasan@gmail.com>
2026-02-20 01:25:39 +0600

    feat(problems): add category and problem models in admin

--- apps/problems/admin.py
+++ apps/problems/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Problem, Category 
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at')
+    list_filter = ('category',)
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
