b447a9e0b2ea8aa8650c55bb95f8772769d1f036
Mahmudul Hasan <mclean.hasan@gmail.com>
2026-02-21 02:24:12 +0600

    feat(problems): add ProblemAdminForm to implement AceWidget highlighting

--- apps/problems/admin.py
+++ apps/problems/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from .models import Problem, Category 
+from django_ace import AceWidget 
+from django import forms 
+
+class ProblemAdminForm(forms.ModelForm):
+    class Meta:
+        model = Problem 
+        fields = '__all__'
+        widgets = {
+            'solution': AceWidget(
+                mode='c_cpp',
+                theme='monokai',
+                width='100%',
+                height='400px',
+                showprintmargin=False,
+                tabsize=4,
+            ),
+        }
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,6 +25,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Problem)
 class ProblemAdmin(admin.ModelAdmin):
+    form = ProblemAdminForm 
     list_display = ('title', 'category', 'created_at')
     list_filter = ('category',)
     search_fields = ('title', 'description')
