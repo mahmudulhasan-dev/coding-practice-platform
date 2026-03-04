9cfdb7c44d0c0a06dae33e0b1296b3cb62c7f3b7
Mahmudul Hasan <mclean.hasan@gmail.com>
2026-03-05 01:18:05 +0600

    refactor(routing): redirect root URL to problem_list for easier access

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include 
+from django.views.generic.base import RedirectView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/problems/', permanent=True)),
     path('api/users/', include('apps.users.urls')),
     path('problems/', include('apps.problems.urls')),
 ]
